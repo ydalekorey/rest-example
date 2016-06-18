0c04c67d567057f7d4934bf8ce63f723a09cf404
Yuriy Dalekorey <ydalekorei@remit.se>
2016-06-19 00:40:50 +0300

    changed scenario steps to use new functionality for stopping/starting server

--- test/features/CreateProductStepDefinitions.scala
+++ test/features/CreateProductStepDefinitions.scala
@@ -1,60 +1,54 @@
 package features
 
+import test.application.{ProductsRepositoryComponent, ProductsRestApi}
 import cucumber.api.scala.{EN, ScalaDsl}
-import dal.ProductsRepository
 import models.Product
 import org.scalatest.ShouldMatchers
-import play.api.http.Status._
 import play.api.libs.json.{JsValue, Json, Writes}
 import play.api.libs.ws.WSResponse
+import play.api.test.Helpers._
 
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
-
-class CreateProductStepDefinitions extends ScalaDsl with EN with PlaySteps with ShouldMatchers {
+object CreateProductStepDefinitions extends ProductsRepositoryComponent with ScalaDsl with EN with ShouldMatchers {
 
   private var productDataJson: JsValue = _
 
   private var createResponse: WSResponse = _
 
+  val testServerAddress = s"http://localhost:$testServerPort"
+
   Given("""^that I am passing valid (.*) , (.*) and (.+)$""") { (productCode: String, productName: String, price: Double) =>
     productDataJson = toProductDataJson(productCode, productName, price)
   }
 
-  When("""^I attempt to add this data to the product catalogue$"""){ () =>
-    createResponse = post("/products", productDataJson)
+  When("""^I attempt to add this data to the product catalogue$""") { () =>
+    createResponse = ProductsRestApi.create(productDataJson)
   }
 
-  Then("""^I receive a success message$"""){ () =>
+  Then("""^I receive a success message$""") { () =>
     createResponse.status shouldBe CREATED
     createResponse.body shouldBe "Product successfully saved"
   }
 
-  Then("""^the data has been entered into the database\.$"""){ () =>
-    val productsRepository = injector.instanceOf(classOf[ProductsRepository])
-
-    val savedProduct = Await.result(productsRepository.findByCode((productDataJson\"code").as[String]), Duration.Inf).get
+  Then("""^the data has been entered into the database\.$""") { () =>
+    val savedProduct = await(productsRepository.findByCode((productDataJson \ "code").as[String])).get
 
     savedProduct should equal(productDataJson.as[Product])
   }
 
-  Given("""^that I am passing valid (.*) and (.*) but invalid (.*)$"""){ (productCode: String, productName: String, price: String) =>
+  Given("""^that I am passing valid (.*) and (.*) but invalid (.*)$""") { (productCode: String, productName: String, price: String) =>
     productDataJson = toProductDataJson(productCode, productName, price)
   }
-  Then("""^I receive an appropriate error response$"""){ () =>
+  Then("""^I receive an appropriate error response$""") { () =>
     createResponse.status shouldBe BAD_REQUEST
     createResponse.body shouldBe "Invalid product data"
   }
-  Then("""^the data has NOT been entered into the database\.$"""){ () =>
-
-    val productsRepository = injector.instanceOf(classOf[ProductsRepository])
-
-    val savedProduct = Await.result(productsRepository.findByCode((productDataJson\"code").as[String]), Duration.Inf)
+  Then("""^the data has NOT been entered into the database\.$""") { () =>
+    val savedProduct = await(productsRepository.findByCode((productDataJson \ "code").as[String]))
 
     savedProduct should be(None)
   }
 
-  private def toProductDataJson[P:Writes](productCode:String, productName:String, price: P) =
-    Json.obj("code" -> productCode, "name"->productName, "price"-> price)
+  private def toProductDataJson[P: Writes](productCode: String, productName: String, price: P) =
+    Json.obj("code" -> productCode, "name" -> productName, "price" -> price)
 
 }
--- test/features/DeleteProductStepDefinitions.scala
+++ test/features/DeleteProductStepDefinitions.scala
@@ -1,45 +1,37 @@
 package features
 
+import test.application.{ProductsRepositoryComponent, ProductsRestApi}
 import cucumber.api.scala.{EN, ScalaDsl}
-import dal.ProductsRepository
 import models.Product
 import org.scalatest.ShouldMatchers
-import play.api.http.Status._
-import play.api.libs.json.{JsValue, Json, Writes}
 import play.api.libs.ws.WSResponse
+import play.api.test.Helpers._
 
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
-
-class DeleteProductStepDefinitions extends ScalaDsl with EN with PlaySteps with ShouldMatchers {
+object DeleteProductStepDefinitions extends ProductsRepositoryComponent with ScalaDsl with EN with ShouldMatchers {
 
   private var productCodeToRemove: String = _
 
   private var createResponse: WSResponse = _
 
   Given("""^that a valid product with (.*) , (.*) and (.+) exists in catalogue$"""){ (productCode: String, productName: String, price: Double) =>
-    val productsRepository = injector.instanceOf(classOf[ProductsRepository])
-
-    Await.result(productsRepository.create(new Product(productCode, productName, price)), Duration.Inf)
+    await(productsRepository.create(new Product(productCode, productName, price)))
   }
 
-  Given("""^that I am passing valid (.*)$""") { (productCode: String) =>
+  Given("""^that I am passing valid (.*)\.$""") { (productCode: String) =>
     productCodeToRemove = productCode
   }
 
   When("""^I attempt to delete the related data in the product catalogue$"""){ () =>
-    createResponse = delete(s"/products/$productCodeToRemove")
+    createResponse = ProductsRestApi.delete(productCodeToRemove)
   }
 
-  Then("""^I receive a success message$"""){ () =>
+  Then("""^I receive a delete succeeded message$"""){ () =>
     createResponse.status shouldBe OK
     createResponse.body shouldBe "Product successfully removed"
   }
 
   Then("""^the data has been deleted in the database\.$"""){ () =>
-    val productsRepository = injector.instanceOf(classOf[ProductsRepository])
-
-    val savedProduct = Await.result(productsRepository.findByCode(productCodeToRemove), Duration.Inf)
+    val savedProduct = await(productsRepository.findByCode(productCodeToRemove))
 
     savedProduct should be(None)
   }
