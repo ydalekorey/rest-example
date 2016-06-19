0369b1c73012a711fcb50094ff8dfa5da13a86e4
Yuriy Dalekorey <ydalekorei@remit.se>
2016-06-19 13:24:02 +0300

    Added test to verify deleting

--- test/dal/slick/ProductRepositorySpec.scala
+++ test/dal/slick/ProductRepositorySpec.scala
@@ -2,27 +2,24 @@ package dal.slick
 
 import models.Product
 import org.scalatest.Matchers._
-import org.scalatest.{BeforeAndAfter, TestData}
+import org.scalatest.TestData
 import org.scalatestplus.play._
 import play.api.Application
 import play.api.test.FakeApplication
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
-
 import test.application.DbHelpers._
 
-class ProductRepositorySpec extends PlaySpec with OneAppPerTest with BeforeAndAfter {
-
-  var productsRepository: ProductsRepository = _
+class ProductRepositorySpec extends PlaySpec with OneAppPerTest {
 
   override def newAppForTest(testData: TestData): Application = FakeApplication(additionalConfiguration = inMemoryDb())
+
+  def productsRepository = app.injector.instanceOf[ProductsRepository]
 
   "ProductsRepository" must {
 
     "find Product by by code" in {
-
-      productsRepository = app.injector.instanceOf[ProductsRepository]
 
       val expectedProduct = Product("FG001", "Red Umbrella", 12.59)
 
@@ -31,6 +28,19 @@ class ProductRepositorySpec extends PlaySpec with OneAppPerTest with BeforeAndAf
       val actualProduct = Await.result(productsRepository.findByCode(expectedProduct.code), Duration.Inf).get
 
       actualProduct should equal (expectedProduct)
+    }
+
+    "delete Product by by code" in {
+
+      val productToRemove = Product("FG001", "Red Umbrella", 12.59)
+      Await.result(productsRepository.create(productToRemove), Duration.Inf)
+
+
+      Await.result(productsRepository.delete(productToRemove.code), Duration.Inf)
+
+      val actualProduct = Await.result(productsRepository.findByCode(productToRemove.code), Duration.Inf)
+
+      actualProduct should be(None)
     }
 
   }
