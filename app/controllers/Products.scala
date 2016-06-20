523374215f02c149b53b26f5203e856f18dfb073
Yuriy Dalekorey <ydalekorei@remit.se>
2016-06-20 03:53:54 +0300

    Integrated swagger

--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -3,6 +3,7 @@ package controllers
 import javax.inject._
 
 import dal.ProductsRepository
+import io.swagger.annotations._
 import models.Product
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json.Json
@@ -11,7 +12,29 @@ import play.api.mvc.{Action, Controller}
 import scala.concurrent.Future
 
 @Singleton
+@Api(value = "/products", description = "Operations with Products")
 class Products @Inject()(productsRepository: ProductsRepository) extends Controller {
+
+  @ApiOperation(
+    nickname = "createProduct",
+    value = "Create Product",
+    notes = "Create Product record in catalogue",
+    httpMethod = "POST"
+  )
+  @ApiImplicitParams(
+    Array(
+      new ApiImplicitParam(
+        name = "body",
+        dataType = "models.Product",
+        required = true,
+        paramType = "body",
+        value = "Product"
+      )
+    )
+  )
+  @ApiResponses(Array(
+    new ApiResponse(code = 400, message = "Invalid product data"),
+    new ApiResponse(code = 201, message = "Product successfully saved")))
   def create = Action.async(parse.json) { implicit request =>
 
     request.body.validate[Product].fold(
@@ -26,19 +49,54 @@ class Products @Inject()(productsRepository: ProductsRepository) extends Control
     )
   }
 
-  def delete(code: String) = Action.async { implicit request =>
+  @ApiOperation(
+    nickname = "deleteProduct",
+    value = "Delete Product",
+    notes = "Delete Product record in catalogue",
+    httpMethod = "DELETE"
+  )
+  @ApiResponses(Array(
+    new ApiResponse(code = 200, message = "Product successfully deleted")))
+  def delete(@ApiParam(value = "Code of product to remove") code: String) = Action.async { implicit request =>
     productsRepository.delete(code).map { _ =>
       Ok("Product successfully deleted")
     }
   }
 
-  def get(code: String) = Action.async {
+  @ApiOperation(
+    nickname = "getProduct",
+    value = "Get Product",
+    notes = "Get Product record by code from catalogue",
+    httpMethod = "GET"
+  )
+  @ApiResponses(Array(
+    new ApiResponse(code = 200, message = "Product found", response = classOf[Product])))
+  def get(@ApiParam(value = "Code of product to retrieve") code: String) = Action.async {
     productsRepository.findByCode(code).map { product =>
       Ok(Json.toJson(product))
     }
   }
 
-  def update(code: String) = Action.async(parse.json) { implicit request =>
+  @ApiOperation(
+    nickname = "updateProduct",
+    value = "Update Product",
+    notes = "Update Product record by code in catalogue",
+    httpMethod = "PUT"
+  )
+  @ApiImplicitParams(
+    Array(
+      new ApiImplicitParam(
+        name = "body",
+        dataType = "models.Product",
+        required = true,
+        paramType = "body",
+        value = "Product"
+      )
+    )
+  )
+  @ApiResponses(Array(
+    new ApiResponse(code = 200, message = "Product successfully updated")))
+  def update(@ApiParam(value = "Code of product to update")code: String) = Action.async(parse.json) { implicit request =>
     val product = request.body.as[Product]
     productsRepository.updateByCode(code, product).map { _ =>
       Ok("Product successfully updated")
--- build.sbt
+++ build.sbt
@@ -6,7 +6,13 @@ moduleName := "scala-product-catalogue-coding-test"
 
 version := "1.0.0-SNAPSHOT"
 
-lazy val root = (project in file(".")).enablePlugins(PlayScala)
+/*At the time of this writing, support for Play 2.5 was not available in the official swagger-play repository.
+This repository is a copy of the Play-2.5 folder in pikel's fork.
+It was created this way to make it easier to add as a git dependency.
+WARNING: This repository will be discarded once support is added to the official swagger-play repo.*/
+lazy val swagger = RootProject(uri("https://github.com/CreditCardsCom/swagger-play.git"))
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala).dependsOn(swagger)
 
 scalaVersion := "2.11.7"
 
@@ -15,6 +21,7 @@ libraryDependencies ++= Seq(
   "com.typesafe.play" %% "play-slick" % "2.0.0",
   "com.typesafe.play" %% "play-slick-evolutions" % "2.0.0",
   "com.h2database" % "h2" % "1.4.187",
+
   "org.mockito" % "mockito-core" % "1.10.19" % Test,
   "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test,
   "info.cukes" %% "cucumber-scala" % "1.2.4" % Test,
